a09807514327f0197a616e4c4523578ae4ba048e
PnPH <mdbslash@hotmail.it>
2016-10-06 21:12:44 +0200

    Create openDNS-IP-Updater.py

---
+++ openDNS-IP-Updater.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import shlex, subprocess, urllib, urllib.request, time, datetime;
+
+
+while True:
+    def internet_on(reference):
+        try:
+            urllib.request.urlopen(reference, timeout=1)
+            return True
+        except  urllib.request.URLError:
+            return False
+
+        while internet_on("http://www.opendns.com/")==False: #Keep trying until a connection with the Internet is connected.
+            time.sleep(9);
+
+    #Update opendns IP address
+    command=shlex.split("curl -u mail:password https://updates.opendns.com/nic/update");
+    subprocess.call(command);
+    print("");
+    tempo = time.asctime()
+    print(tempo)
+    time.sleep(1*60*60) 
